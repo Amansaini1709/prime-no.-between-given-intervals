d2adca498440f980c73408ce0fcb2df1291a4855
Jitesh Chawla <71929662+jiteshchawla1511@users.noreply.github.com>
2020-09-30 20:59:28 +0530

    Update primeno_bw_given_interval.py

--- primeno_bw_given_interval.py
+++ primeno_bw_given_interval.py
@@ -1,7 +1,7 @@
 #prime no. in given intervals
 a=int(input("enter the first interval"))
 b=int(input("enter the second interval"))
-for i in range(a+1,b,1):
+for i in range(a,b,1):
     for d in range (2,i,1):
         if i%d=0:
             break
